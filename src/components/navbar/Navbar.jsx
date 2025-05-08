f7da37c142d2be9d2305ba48b25439e19e99a244
fareed gul <fareedgul735@gmail.com>
2025-05-08 23:10:36 +0500

    add my all changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "../navbar/logo.png";
+import avatar from "../navbar/profile-logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -9,14 +10,16 @@ const Navbar = () => {
           <img src={logo} alt="navbar-logo" width={"90px"} height={"90px"} />
         </div>
         <div className="bell-icon">
-          <i class="fa-solid fa-bell"></i>
+          <i className="fa-solid fa-bell"></i>
         </div>
       </div>
       <div className="parent2">
         <div className="nav-btn">
           <button>Learning Free</button>
         </div>
-        <div className="profile">Hi</div>
+        <div className="profile">
+          <img className="profile-avatar" src={avatar} alt="profile-avatar" />
+        </div>
       </div>
     </nav>
   );
